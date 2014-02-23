2d758c1676d540c9ae6b97232c73b0b423279df5
Mike Haas <mike@emken.co>
2014-02-23 16:42:16 -0600

    Switch assertion library to Chai.

--- test/rangify-spec.js
+++ test/rangify-spec.js
@@ -1,9 +1,20 @@
-var expect = require('expect.js'),
+var expect = require('chai').expect,
     rangify = require('..');
 
 describe('rangifyjs', function() {
   it('should convert string to array', function(done) {
     expect(rangify('1,2-3,5-6,7,9')).to.eql([1,2,3,5,6,7,9]);
     done();
+  });
+
+  it('should reduce an array to string representation', function(done) {
+    expect(rangify([3, 4, 5, 1, 2, 6, 9, 11, 12])).to.eql('1-6,9,11-12');
+    done();
+  });
+
+  it('should throw an error if neither an array or string is passed to rangify() facade.', function() {
+    expect(function() { rangify({}); }).to.throw();
+    expect(function() { rangify([]); }).to.not.throw();
+    expect(function() { rangify(''); }).to.not.throw();
   });
 });
